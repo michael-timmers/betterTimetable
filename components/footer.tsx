ca44b8da6c2e8a31db1c5c672c46f34a6c19440a
zac-k-03 <n11249692@qut.edu.au>
2025-02-27 01:37:13 +1000

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,12 @@
 export default function Footer() {
-    return (
-      <footer className="bg-base-100 text-center py-4">
-        <p className="text-gray-300">© 2025 Code Network. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="bg-base-100 bg-gray-900 text-center py-4 h-48">
+      <p className="text-gray-300">© 2025 Code Network. All rights reserved.</p>
+      <div className="mt-6">
+        <a href="/about" className="text-gray-300 mx-2 hover:text-white">About Us</a>
+        <a href="/contact" className="text-gray-300 mx-2 hover:text-white">Contact</a>
+        <a href="/privacy-policy" className="text-gray-300 mx-2 hover:text-white">Privacy Policy</a>
+      </div>
+    </footer>
+  );
+}

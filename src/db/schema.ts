0ff0fffd588470011858a37a9ea5d56b7c14e3a9
MichaelTimmers6 <97582970+MichaelTimmers6@users.noreply.github.com>
2025-03-04 11:59:04 +1000

    Add overview of possible new DB tables for units and classes

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,6 +3,16 @@
 import { mysqlTable, mysqlSchema, varchar, boolean, text, int, timestamp, primaryKey } from "drizzle-orm/mysql-core";
 import { sql } from "drizzle-orm";
 
+//Schema overview
+//users(_id#, _userName, title, firstName, lastName, email, admin)
+//userPwdResets(_id#, userId#, code, expiresAt)
+//sessions(_id#, userId#, expiresAt)
+//units(_id#, unitCode, unitName)
+//classes(_id#, classTyhpe, activity, day, time, room, teachingStaff)
+//userClasses(userId#, classId#)
+//
+//perhaps teaching staff could be split as well, however this does not seem to be that useful for the appllication.
+
 // Users Table
 export const users = mysqlTable("users", {
   id: varchar("id", { length: 36 }).primaryKey().default(sql`(UUID())`),

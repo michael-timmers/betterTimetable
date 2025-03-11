c603ee6f4a1f90b28092d6b91b785c671fed5bf0
zac-k-03 <n11249692@qut.edu.au>
2025-03-12 00:50:58 +1000

    Updated nav bar
    - Added authenticated response to plan page
    - Updated navbar links

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -1,13 +1,31 @@
 "use server";
 
 import React from 'react';
-import planData from './planData'; // Make sure planData is a function returning a promise
+import planData from './planData'; // Ensure planData is a function returning a promise
 import ClientComponent from './details'; // Adjust import path accordingly
+import { validateSession } from "../../auth/sessionManager";
 
 export default async function ServerComponent() {
+    const { user } = await validateSession();
+
     // Assuming planData is an async function that returns data
-    const data = await planData(); 
+    const data = await planData();
 
     // Render the ClientComponent with the fetched data
-    return <ClientComponent data={data} />;
+    return (
+        <>
+            <p>
+                The following user has logged in:{" "}
+                {user ? (
+                    <>
+                        {user.firstName} {user.lastName}
+                    </>
+                ) : (
+                    "Guest"
+                )}
+            </p>
+            <br></br>
+            <ClientComponent data={data} />
+        </>
+    );
 }
